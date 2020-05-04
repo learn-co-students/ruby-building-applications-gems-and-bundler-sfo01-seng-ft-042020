819904ff998057d430293c39ffa38501e43ce1e8
github username <github email address>
2020-05-03 20:10:39 -0700

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,17 @@
 # require the environment file
+
+# require_relative '../config/environment'
+
+
+
+#############################
+
+
+  
+require_relative '../config/environment'
+
+Hashie
+Sinatra
+Octokit
+Pry
+AwesomePrint
